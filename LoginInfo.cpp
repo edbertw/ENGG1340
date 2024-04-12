f199586d1f105b0d0c7088aac73668b44b1f21e9
Edbert Widjaja <157877834+edbertw@users.noreply.github.com>
2024-04-12 14:09:26 +0800

    Update LoginInfo.cpp

--- LoginInfo.cpp
+++ LoginInfo.cpp
@@ -1,1 +1,18 @@
+#include <ncurses.h>
+#include <string>
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <cstdlib>
+#include <cstring>
+using namespace std;
 
+string filename="users.txt";
+struct User {
+    string username;
+    string password;
+    int highestScore;
+};
+
+User currentUser;
