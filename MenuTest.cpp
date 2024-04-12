bfa82658f99a33ba0a45188ff8c4fe1c76b86424
Edbert Widjaja <157877834+edbertw@users.noreply.github.com>
2024-04-12 14:11:18 +0800

    Update MenuTest.cpp

--- MenuTest.cpp
+++ MenuTest.cpp
@@ -1,1 +1,17 @@
+#include <ncurses.h>
+#include <string>
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <cstdlib>
+using namespace std;
 
+string filename="users.txt";
+struct User {
+    string username;
+    string password;
+    int highestScore;
+};
+
+User currentUser;
