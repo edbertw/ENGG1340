94fb0626d025a5e2ac1db07971d46115ac738e0b
Edbert Widjaja <157877834+edbertw@users.noreply.github.com>
2024-04-12 14:28:37 +0800

    Update LoginInfo.cpp

--- LoginInfo.cpp
+++ LoginInfo.cpp
@@ -16,3 +16,33 @@ struct User {
 };
 
 User currentUser;
+void displayMenuPage();
+
+//The PrintCenter function prints the text to be displayed at center of screen
+void PrintCenter(const char* format, ...) { //The input parameters are a format string to specify the desired format and text of the output
+    // Outputs are in the form of the string to be displayed
+    int screenWidth, screenHeight;
+    getmaxyx(stdscr, screenHeight, screenWidth); // Get the screen size
+
+    va_list args;
+    va_start(args, format);
+
+    // We need to calculate the length of the formatted string.
+    // vsnprintf with nullptr and 0 as buffer and size will return the required length for the formatted string.
+    int len = vsnprintf(nullptr, 0, format, args) + 1; // +1 for '\0'
+    va_end(args);
+
+    // Allocate memory for the formatted string
+    char* str = new char[len];
+
+    // Format the string
+    va_start(args, format);
+    vsnprintf(str, len, format, args);
+    va_end(args);
+
+    // print the formatted string at the center of the screen
+    mvprintw(screenHeight / 2, (screenWidth - strlen(str)) / 2, "%s", str);
+
+    // Clean up memory
+    delete[] str;
+}
