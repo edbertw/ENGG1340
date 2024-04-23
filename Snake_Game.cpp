e9471dec1ff59c864f87ff820e9c2cae82b76183
HowardA05 <159270520+HowardA05@users.noreply.github.com>
2024-04-23 20:20:34 +0800

    Update Snake_Game.cpp

--- Snake_Game.cpp
+++ Snake_Game.cpp
@@ -16,26 +16,269 @@ struct User { // Make user struct. Refers to the structure of user, pass, and sc
 
 User currentUser; // Make variable for currentUser, will be updated during gamerun.
 
-// PUT THE OTHER FUNCTIONS BROSSSSS. Change the function or variable name if needed, this is the main algorithm though.
-void Setup() {
-  
+void InitColors() {
+    if (has_colors() == FALSE) {
+        endwin();
+        printf("Your terminal does not support color\n");
+        exit(1);
+    }
+    start_color();
+    init_pair(1, COLOR_RED, COLOR_BLACK); // Red on black text
+    init_pair(2, COLOR_CYAN, COLOR_BLACK); // Cyan on black text
+    init_pair(3, COLOR_GREEN, COLOR_BLACK); // Green on black text
 }
 
+// PUT THE OTHER FUNCTIONS BROSSSSS. Change the function or variable name if needed, this is the main algorithm though.
+void Setup() {
+    initscr(); 
+    clear(); 
+    noecho(); 
+    cbreak(); 
+    curs_set(0); 
+    // These functions above starts curse mode and does all the things to make the UI clean, such as clearing screen, hifing cursore, disabling line buffering, etc.
+
+    gameOver = false;
+    restart = false;
+    dir = STOP;
+    x = width / 2;
+    y = height / 2;
+    fX = rand() % width;
+    fY = rand() % height;
+    score = 0;
+    tail.clear();
+    blocks.clear();
+
+    keypad(stdscr, TRUE); // Enable user to use arrow keys
+    nodelay(stdscr, TRUE); // Make getch() non-blocking
+    timeout(100); 
+} // function that sets up the cursed environment
+
 void Draw(){
-  
+    clear(); 
+    InitColors();
+
+    
+    int maxWidth, maxHeight;
+    getmaxyx(stdscr, maxHeight, maxWidth);
+
+    // Calculate starting positions for centered board
+    int startCol = (maxWidth - width - 2) / 2; // -2 is for the left and right walls
+    int startRow = (maxHeight - height - 2) / 2; // -2 is for the top and bottom walls
+
+    // Draw top wall
+    for (int i = 0; i < width + 2; i++)
+        mvprintw(startRow, startCol + i, "#");
+ for (int i = 0; i < height; i++) {
+        mvprintw(startRow + i + 1, startCol, "#"); // Draw Left wall
+        for (int j = 0; j < width; j++) {
+            if (i == y && j == x) {
+	        attron(COLOR_PAIR(3));
+                mvprintw(startRow + i + 1, startCol + j + 1, "O"); // Snake head
+		attroff(COLOR_PAIR(3));
+            } else if (i == fY && j == fX) {
+	        attron(COLOR_PAIR(1));
+                mvprintw(startRow + i + 1, startCol + j + 1, "F"); // Fruit
+                attroff(COLOR_PAIR(2));
+	    } else {
+                bool print = false;
+                for (auto t : tail) {
+                    if (t.first == j && t.second == i) {
+		        attron(COLOR_PAIR(3));
+                        mvprintw(startRow + i + 1, startCol + j + 1, "o"); // Snake tail
+                        attroff(COLOR_PAIR(3));
+			print = true;
+                        break;
+                    }
+                }
+                for (auto& block : blocks) {
+                    for (int bx=0;bx<2;bx++) {
+		        for (int by=0;by<2;by++) {
+			    if (block.first+bx==j && block.second+by==i) {
+			        attron(COLOR_PAIR(2));
+			        mvprintw(startRow+i+1, startCol+j+1, "#");
+				attroff(COLOR_PAIR(2));
+				print=true;
+			    }
+			}
+			if (print) break;
+		    }
+		if (print) break;
+	        }
+	    }
+	}
+	
+        mvprintw(startRow + i + 1, startCol + width + 1, "#"); // Draw Right wall
+    }
+
+    // Draw bottom wall
+    for (int i = 0; i < width + 2; i++)
+        mvprintw(startRow + height + 1, startCol + i, "#");
+
+    // Score text printed below bottom wall, centred.
+    char scoreText[20];
+    sprintf(scoreText, "Score: %d", score);
+    mvprintw(startRow + height + 3, (maxWidth - strlen(scoreText)) / 2, "%s", scoreText);
+
+    char highScoreText[50];
+    sprintf(highScoreText, "High Score: %d", currentUser.highestScore);
+
+    // Highscore text printed below score text
+    mvprintw(startRow +height +5, (maxWidth - strlen(highScoreText)) / 2, "%s", highScoreText);
+
+
+    refresh(); // Print it on to the real screen
 }
 
 void Input() {
   
 }
 
-void Logic(){
-  
+void Logic() {
+    int prevX = x;
+    int prevY = y;
+    int prev2X, prev2Y;
+    switch (dir) {
+    case LEFT:
+        x--;
+        break;
+    case RIGHT:
+        x++;
+        break;
+    case UP:
+        y--;
+        break;
+    case DOWN:
+        y++;
+        break;
+    default:
+        break;
+    }
+    // Check collision with wall
+    if (x<0 || x>= width || y<0 || y>= height) {
+    	gameOver=true;
+	}
+    // Check collision with snake's own tail
+    for (auto t : tail) {
+        if (t.first == x && t.second == y) {
+            gameOver = true;
+        }
+    }
+
+    //Check for collision with blocks
+    for (auto& block : blocks) {
+        for (int i=0;i<2;i++) {
+	    for (int j=0; j<2; j++) {
+	        if (x==block.first+i && y==block.second+j) {
+		    gameOver=true;
+		}
+	    }
+	}
+    }
+    // fruit collection
+    if (x == fX && y == fY) {
+        score += 10; 
+        spawnFruit();  // If fruit collected, score added, new fruit and new block spawned
+	    spawnBlock(tail, x, y, fX, fY);
+        growTail = true; // Then add a new segment to the tail at the current position
+	    sleepDuration = max(10, sleepDuration-7);
+    }
+
+    if (growTail) {
+        pair<int,int> newSegment = make_pair(prevX, prevY);
+	    tail.insert(tail.begin(), newSegment);
+	    growTail=false;
+    } else if (!tail.empty()) {
+        // Move tail segments forward
+        prev2X = tail[0].first;
+        prev2Y = tail[0].second;
+        tail[0].first = prevX;
+        tail[0].second = prevY;
+        for (size_t i = 1; i < tail.size(); i++) {
+            prevX = prev2X;
+            prevY = prev2Y;
+            prev2X = tail[i].first;
+            prev2Y = tail[i].second;
+            tail[i].first = prevX;
+            tail[i].second = prevY;
+        }
+    }
 }
 
+
 void GameOver(){
-	
-}
+    clear(); // Clear the screen
+    int maxWidth, maxHeight;
+    getmaxyx(stdscr, maxHeight, maxWidth); // Finds the maximum width and height of the user's terminal window
+
+
+    const char* gameOverText[] = {
+    
+"  _____                         ____                 ",
+" / ____|                       / __ `                ",
+"| |  __  __ _ _ __ ___   ___  | |  | |_   _____ _ __ ",
+"| | |_ |/ _` | '_ ` _ ` / _ ` | |  | ` ` / / _ ` '__|",
+"| |__| | (_| | | | | | |  __/ | |__| |` V /  __/ |   ",
+" `_____|`__,_|_| |_| |_|`___|  `____/  `_/ `___|_|   ",
+    
+    };
+
+    int gameOverTextLines = sizeof(gameOverText) / sizeof(gameOverText[0]);
+    int longestLineLength = 0;
+    for (int i = 0; i < gameOverTextLines; ++i) {
+        longestLineLength = max(longestLineLength, static_cast<int>(strlen(gameOverText[i])));
+    }
+    InitColors();
+    int startY = (maxHeight - gameOverTextLines - 14) / 2;
+    for (int i = 0; i < gameOverTextLines; ++i) { // 8 is the number of lines in gameOverText
+        int startX = (maxWidth - strlen(gameOverText[i])) / 2; // Centering horizontally
+        if (i==0 || i == 1 || i == 2 || i == 3) { // Lines that contain "Game"
+            attron(COLOR_PAIR(1)); // Red
+            mvprintw(startY + i, startX, "%s", gameOverText[i]);
+            attroff(COLOR_PAIR(1));
+        } else if (i == 4 || i == 5) { // Lines that contain "Over"
+            attron(COLOR_PAIR(2)); // Blue, assuming init_pair(2, COLOR_BLUE, COLOR_BLACK) in InitColors()
+            mvprintw(startY + i, startX, "%s", gameOverText[i]);
+            attroff(COLOR_PAIR(2));
+        } else { // For lines that don't specifically contain "Game" or "Over" text
+            mvprintw(startY + i, startX, "%s", gameOverText[i]);
+        }
+    }
+    
+    char scoreText[20];
+    sprintf(scoreText, "Score: %d", score);
+
+    // Print the score centered
+    mvprintw(maxHeight / 2, (maxWidth - strlen(scoreText)) / 2, "%s", scoreText);
+
+    char highScoreText[50];
+    sprintf(highScoreText, "High Score: %d", currentUser.highestScore);
+    mvprintw(maxHeight/2+2, (maxWidth - strlen(highScoreText))/2, "%s", highScoreText);
+
+    // Print the restart or quit text at the center
+    const char* restartQuitText = "Press 'r' to restart or 'q' to quit.";
+    mvprintw((maxHeight / 2) + 4, (maxWidth - strlen(restartQuitText)) / 2, "%s", restartQuitText);
+
+    refresh(); // Refresh to show text
+
+    // Wait for 'q' to be pressed before exiting
+    nodelay(stdscr, FALSE);
+    // Now, wait for the user input for 'r' or 'q'
+    char ch;
+    do {
+        ch = getch(); // Wait for user input
+        if (ch == 'q') {
+            gameOver = true; 
+            restart = false; 
+            break;
+        } else if (ch == 'r') {
+            gameOver = false; 
+            restart = true; 
+            break;
+        }
+    } while(true);
+
+    clear(); // Optionally clear the screen before restarting or exiting
+} // Game Over function that displays user's score, highscore, and prompts the user to either quit the game or restart the game
 
 void updateHighestScore(){
 	
